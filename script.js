fcacaf8946938641e3f3325d94953bfa9e83ffeb
Ing-Estefania-Pachon <ing.estefaniapachon@gmail.com>
2025-07-21 23:33:03 -0500

    Agregue los estilos y el archivo HTML

--- script.js
+++ script.js
@@ -2,47 +2,86 @@
 // Debe solicitar al usuario 3 números por prompt y guardarlos en sus respectivas variables.
 // Debe analizar los números, identificar cual es el número mayor, el número del centro y el número menor.
 
-import promptSync from "prompt-sync";
-const prompt = promptSync();
+//*Código para solo js
+// import promptSync from "prompt-sync";
+// const prompt = promptSync();
 
-function organizarNumeros(){
+// function organizarNumeros(){
 
-    let listaNumeros = []
+//     let listaNumeros = []
 
-    console.log("Orgnaizador de números");
-    console.log("\nSi no quieres ingresar más número coloca cualquier letra");
+//     console.log("Orgnaizador de números");
+//     console.log("\nSi no quieres ingresar más número coloca cualquier letra");
 
-    while (true){
+//     while (true){
         
-        let numero = prompt("Ingresé un número: ");
-        let valorNum = parseInt(numero);
+//         let numero = prompt("Ingresé un número: ");
+//         let valorNum = parseInt(numero);
 
 
-        if (!isNaN(valorNum)){
-            listaNumeros.push(valorNum);
-        }else {
-            break
-        }
+//         if (!isNaN(valorNum)){
+//             listaNumeros.push(valorNum);
+//         }else {
+//             break
+//         }
 
+//     }
+
+//     if (listaNumeros.length > 0){
+//         for (let i = 0; i < listaNumeros.length; i++) {
+//             for (let j = 0; j < listaNumeros.length - 1; j++) {
+//                 if (listaNumeros[j] > listaNumeros[j + 1]) {
+//                     // Intercambiar
+//                     let temp = listaNumeros[j];
+//                     listaNumeros[j] = listaNumeros[j + 1];
+//                     listaNumeros[j + 1] = temp;
+//                 }
+//             }
+//         }
+
+//         console.log("Números ordenados de menor a mayor:");
+//         console.log(listaNumeros);
+
+//     }
+
+// }
+
+// organizarNumeros()
+
+let listaNumeros = []
+
+document.getElementById("agregar").addEventListener("click", function() {
+    const input = document.getElementById("nuevo-numero");
+    const nuevoNumero = parseInt(input.value);
+
+    if (!isNaN(nuevoNumero)){
+        listaNumeros.push(nuevoNumero);
+        input.value = "";
+        alert("Número " + nuevoNumero + " agregado.");
+        
+    }else{
+        alert("Por favor ingresar un número válido");
     }
 
-    if (listaNumeros.length > 0){
-        for (let i = 0; i < listaNumeros.length; i++) {
-            for (let j = 0; j < listaNumeros.length - 1; j++) {
-                if (listaNumeros[j] > listaNumeros[j + 1]) {
-                    // Intercambiar
-                    let temp = listaNumeros[j];
-                    listaNumeros[j] = listaNumeros[j + 1];
-                    listaNumeros[j + 1] = temp;
-                }
+}   )
+
+document.getElementById("ordenar").addEventListener("click", function (){
+    if (listaNumeros.length === 0) {
+        alert("No hay números para ordenar.");
+        return;
+    }
+
+    for (let i = 0; i < listaNumeros.length; i++){
+        for (let j = 0; j < listaNumeros.length - 1; j++) {
+            if (listaNumeros[j] > listaNumeros [j + 1]) {
+                let temporal = listaNumeros [j];
+                listaNumeros[j] = listaNumeros [j + 1];
+                listaNumeros[j + 1] = temporal; 
             }
         }
-
-        console.log("Números ordenados de menor a mayor:");
-        console.log(listaNumeros);
-
     }
 
-}
+    const resultado = document.getElementById("resultado");
+    resultado.innerText = "Números ordenados: " + listaNumeros.join(", ");
 
-organizarNumeros()
+})
